07990d7800a7c35f9c882bd26d0fe609057da954
ericcunha202302 <145268471+ericcunha202302@users.noreply.github.com>
2023-10-29 15:26:38 -0300

    Inserindo tela de oferta

--- codigo-fonte/Artefatos/Home Page/scripts.js
+++ codigo-fonte/Artefatos/Home Page/scripts.js
@@ -7,120 +7,120 @@ document.addEventListener("DOMContentLoaded", function() {
                 "filled": "1 de 5",
                 "profile": "Premium User"
             },
-            {
-                "name": "Ana Silva",
-                "product": "Netflix Premium",
-                "rating": 4,
-                "profile": "Family Plan"
-            },
-            {
-                "name": "Lucas Santos",
-                "product": "Amazon Prime Video",
-                "rating": 4.5,
-                "profile": "Student Subscription"
-            },
-            {
-                "name": "Maria Garcia",
-                "product": "Apple Music",
-                "rating": 4,
-                "profile": "Family Plan"
-            },
-            {
-                "name": "Pedro Oliveira",
-                "product": "HBO Max",
-                "rating": 4.2,
-                "profile": "Premium User"
-            },
-            {
-                "name": "Isabela Costa",
-                "product": "Disney+",
-                "rating": 4.8,
-                "profile": "Standard Subscription"
-            },
-            {
-                "name": "Carlos Rodrigues",
-                "product": "Kindle Unlimited",
-                "rating": 3.9,
-                "profile": "Reader's Delight"
-            },
-            {
-                "name": "Laura Fonseca",
-                "product": "Spotify Family",
-                "rating": 4.7,
-                "profile": "Family Plan"
-            },
-            {
-                "name": "Gustavo Souza",
-                "product": "YouTube Premium",
-                "rating": 4.3,
-                "profile": "Ad-Free Experience"
-            },
-            {
-                "name": "Juliana Pereira",
-                "product": "Tidal HiFi",
-                "rating": 4.6,
-                "profile": "Audiophile"
-            },
-            {
-                "name": "Rafael Fernandes",
-                "product": "Apple TV+",
-                "rating": 4.1,
-                "profile": "Original Content Lover"
-            },
-            {
-                "name": "Camila Lima",
-                "product": "Audible",
-                "rating": 4.4,
-                "profile": "Audiobook Enthusiast"
-            },
-            {
-                "name": "Fernando Torres",
-                "product": "Pandora Plus",
-                "rating": 4,
-                "profile": "Personalized Radio"
-            },
-            {
-                "name": "Mariana Ribeiro",
-                "product": "Sling TV",
-                "rating": 4.2,
-                "profile": "Cord Cutter"
-            },
-            {
-                "name": "André Alves",
-                "product": "PlayStation Plus",
-                "rating": 4.5,
-                "profile": "Gamer's Choice"
-            },
-            {
-                "name": "Beatriz Gomes",
-                "product": "ESPN+",
-                "rating": 4.7,
-                "profile": "Sports Fan"
-            },
-            {
-                "name": "Ricardo Mendes",
-                "product": "Skillshare",
-                "rating": 4.3,
-                "profile": "Lifelong Learner"
-            },
-            {
-                "name": "Tatiana Carvalho",
-                "product": "NBA League Pass",
-                "rating": 4.6,
-                "profile": "Basketball Enthusiast"
-            },
-            {
-                "name": "Hugo Martins",
-                "product": "Crunchyroll Premium",
-                "rating": 4.2,
-                "profile": "Anime Lover"
-            },
-            {
-                "name": "Amanda Sousa",
-                "product": "Shudder",
-                "rating": 4.8,
-                "profile": "Horror Aficionado"
-            }
+            // {
+            //     "name": "Ana Silva",
+            //     "product": "Netflix Premium",
+            //     "rating": 4,
+            //     "profile": "Family Plan"
+            // },
+            // {
+            //     "name": "Lucas Santos",
+            //     "product": "Amazon Prime Video",
+            //     "rating": 4.5,
+            //     "profile": "Student Subscription"
+            // },
+            // {
+            //     "name": "Maria Garcia",
+            //     "product": "Apple Music",
+            //     "rating": 4,
+            //     "profile": "Family Plan"
+            // },
+            // {
+            //     "name": "Pedro Oliveira",
+            //     "product": "HBO Max",
+            //     "rating": 4.2,
+            //     "profile": "Premium User"
+            // },
+            // {
+            //     "name": "Isabela Costa",
+            //     "product": "Disney+",
+            //     "rating": 4.8,
+            //     "profile": "Standard Subscription"
+            // },
+            // {
+            //     "name": "Carlos Rodrigues",
+            //     "product": "Kindle Unlimited",
+            //     "rating": 3.9,
+            //     "profile": "Reader's Delight"
+            // },
+            // {
+            //     "name": "Laura Fonseca",
+            //     "product": "Spotify Family",
+            //     "rating": 4.7,
+            //     "profile": "Family Plan"
+            // },
+            // {
+            //     "name": "Gustavo Souza",
+            //     "product": "YouTube Premium",
+            //     "rating": 4.3,
+            //     "profile": "Ad-Free Experience"
+            // },
+            // {
+            //     "name": "Juliana Pereira",
+            //     "product": "Tidal HiFi",
+            //     "rating": 4.6,
+            //     "profile": "Audiophile"
+            // },
+            // {
+            //     "name": "Rafael Fernandes",
+            //     "product": "Apple TV+",
+            //     "rating": 4.1,
+            //     "profile": "Original Content Lover"
+            // },
+            // {
+            //     "name": "Camila Lima",
+            //     "product": "Audible",
+            //     "rating": 4.4,
+            //     "profile": "Audiobook Enthusiast"
+            // },
+            // {
+            //     "name": "Fernando Torres",
+            //     "product": "Pandora Plus",
+            //     "rating": 4,
+            //     "profile": "Personalized Radio"
+            // },
+            // {
+            //     "name": "Mariana Ribeiro",
+            //     "product": "Sling TV",
+            //     "rating": 4.2,
+            //     "profile": "Cord Cutter"
+            // },
+            // {
+            //     "name": "André Alves",
+            //     "product": "PlayStation Plus",
+            //     "rating": 4.5,
+            //     "profile": "Gamer's Choice"
+            // },
+            // {
+            //     "name": "Beatriz Gomes",
+            //     "product": "ESPN+",
+            //     "rating": 4.7,
+            //     "profile": "Sports Fan"
+            // },
+            // {
+            //     "name": "Ricardo Mendes",
+            //     "product": "Skillshare",
+            //     "rating": 4.3,
+            //     "profile": "Lifelong Learner"
+            // },
+            // {
+            //     "name": "Tatiana Carvalho",
+            //     "product": "NBA League Pass",
+            //     "rating": 4.6,
+            //     "profile": "Basketball Enthusiast"
+            // },
+            // {
+            //     "name": "Hugo Martins",
+            //     "product": "Crunchyroll Premium",
+            //     "rating": 4.2,
+            //     "profile": "Anime Lover"
+            // },
+            // {
+            //     "name": "Amanda Sousa",
+            //     "product": "Shudder",
+            //     "rating": 4.8,
+            //     "profile": "Horror Aficionado"
+            // }
     ];
 
     const proContainer = document.getElementById("dynamic-ads");
